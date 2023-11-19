c7ff4a5bb72cd4a5c6b82239cc6fbfa8db5082a8
WeinkMFG <Manuel.Weinkauf@gmx.de>
2023-11-19 15:41:55 +0100

    Finished code to generate base calendar

--- Calendar_Calculations.R
+++ Calendar_Calculations.R
@@ -1,4 +1,3 @@
-
 # Load packages -----------------------------------------------------------
 
 library(tidyverse)
@@ -7,26 +6,44 @@ library(lubridate)
 
 # Generate raw calendar ---------------------------------------------------
 
-calendar.generation <- function(year = 2023) {
+# Parameters:
+# year: integer value for year in which calendar should start
+
+calendar.generation <- function(year) {
+  # Calculate start and end date for calendar
+  time.of.year <- list()
+  ## Christmas date in current year for start date of calendar
+  time.of.year$this.year$christmas$date <-
+    ymd(paste(year, "-12-24", sep = ""))
+  time.of.year$this.year$christmas$day.of.week <-
+    wday(time.of.year$this.year$christmas$date, week_start = 7)
+  time.of.year$this.year$advent.start <-
+    time.of.year$this.year$christmas$date - days(27 - (7 - time.of.year$this.year$christmas$day.of.week))
+  ## Christmas date in next year for end of calendar
+  time.of.year$next.year$christmas$date <-
+    ymd(paste(year + 1, "-12-24", sep = ""))
+  time.of.year$next.year$christmas$day.of.week <-
+    wday(time.of.year$next.year$christmas$date, week_start = 7)
+  time.of.year$next.year$advent.start <-
+    time.of.year$next.year$christmas$date - days(27 - (7 - time.of.year$next.year$christmas$day.of.week))
+  
   # Generate date range for year
-  day.range <-
-    c(paste(year, "-01-01", sep = ""), paste(year, "-12-31", sep = ""))
+  day.range <- c(
+    time.of.year$this.year$advent.start,
+    time.of.year$next.year$advent.start %m-% days(1)
+  )
   date.metadata <-
     tibble(date = seq(ymd(day.range[1]), ymd(day.range[2]), by = "days"))
   date.metadata <- date.metadata %>%
-    mutate(day.of.week = wday(date), week_start = 1)
+    mutate(day.of.week = wday(date, week_start = 7))
   
-  # Add information on time of year
-  time.of.year <- list()
-  time.of.year$christmas$date <- ymd(paste(year, "-12-24", sep = ""))
-  time.of.year$christmas$day.of.week <- wday(time.of.year$christmas$date, week_start = 7)
-  time.of.year$advent.start <- time.of.year$christmas$date - days(27 - (7 - time.of.year$christmas$day.of.week))
-  
-  date.metadata <- date.metadata %>%
-    mutate(time.of.year = case_when(
-      date >= time.of.year$advent.start & date <= time.of.year$christmas$date ~ "advent time",
-      TRUE ~ "normal time"
-    ))
+  # Return raw calendar
+  return(date.metadata)
 }
 
+# Add information on time of year -----------------------------------------
+
+
+
+calendar.generation(year = 2023)
 
